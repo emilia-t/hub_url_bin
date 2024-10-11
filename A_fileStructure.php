77d61fca9c09c0fa030e47a6a5629aed0e0cac87
emilia-t <sophietwilight@qq.com>
2024-10-12 07:41:47 +0800

    update

---
+++ A_fileStructure.php
@@ -0,0 +1,13 @@
+<?php
+//level             describe                                    *?
+//A                   说明文件                                   *php
+
+//B                   主文件，前后端入口文件           *php
+
+//D                   class文件，前后端依赖文件       *php
+
+//F                    工具文件，独立脚本文件           *php
+
+//J                     数据库文件，数据库模板           *sqlite
+
+//P                     JS文件                                        *js
